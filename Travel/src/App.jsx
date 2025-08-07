0b60120218a45667baa004ebb30f47d9508b224f
Arela-Cristina <instagrandine@gmail.com>
2025-08-07 17:27:09 +0200

    feat: import PostForms to App.jsx and test it

--- Travel/src/App.jsx
+++ Travel/src/App.jsx
@@ -1,31 +1,43 @@
-import { useState } from 'react'
 import './App.css'
+import { useEffect } from 'react'
 import { supabase } from './supabase/posts'
+import  PostForm  from  '../src/components/PostForm'
 
-const newPost = {
-  foto_url: 'https://mi-foto.com/foto.jpg',
-  place: 'Barcelona, España',
-  description: 'Fui a la playa 🏖️',
-  mood_state: 'feliz',
-  spend: 30,
-  tags: ['spiaggia', 'state'],
-}
+
 
 function App() {
 
+  useEffect(() => {
+    const insertPost = async () => {
+      const newPost = {
+        foto_url: 'https://mi-foto.com/foto.jpg',
+        lugar: 'Barcelona, España',
+        description: 'Fui a la playa 🏖️',
+        mood_state: 'feliz',
+        spend: 30,
+        tags: ['playa', 'verano'],
+      }
 
-  const { data, error } = supabase
-    .from('posts')
-    .insert([newPost])
+      const { data, error } = await supabase
+        .from('posts')
+        .insert([newPost])
 
-  if (error) console.error(error)
-  else console.log('Fetch Realizado', data)
+      if (error) console.error('Error insertando post:', error)
+      else console.log('Post insertado correctamente:', data)
+    }
+
+    insertPost()
+  }, [])
 
   return (
+    <>
+      <div className='bg-gray-100 p-4'>
+        <h1 className='text-xl font-bold'>Travel Journal App</h1>
+      </div>
+      <PostForm />
 
-    <div className='bg-gray-100'>
-      Hello World
-    </div>
+    </>
+
   )
 }
 
