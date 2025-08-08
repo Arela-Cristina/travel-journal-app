68f4b03fe598cf7d19be5f0d8a05ff269e1e75dd
Arela-Cristina <instagrandine@gmail.com>
2025-08-08 17:22:22 +0200

    feat: import Provider to App.jsx

--- Travel/src/App.jsx
+++ Travel/src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import  PostProvider  from './Context/PostContextProvider';
 import OutletLayout from './Pages/OutletLayout';
 import Home from './Pages/Home';
 import PostList from './Pages/PostList';
@@ -12,15 +13,20 @@ function App() {
 
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<OutletLayout />}>
-            <Route index element={<Home />} />
-            <Route path="Lista-dei-Post" element={<PostList />} />
-            <Route path="Log-In" element={<PostForm />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <PostProvider>
+
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<OutletLayout />}>
+              <Route index element={<Home />} />
+              <Route path="Lista-dei-Post" element={<PostList />} />
+              <Route path="Log-In" element={<PostForm />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+
+      </PostProvider>
+
 
     </>
 
