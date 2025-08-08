006dc01160be6e6a61e7ea7a710d5111b203668e
Arela-Cristina <instagrandine@gmail.com>
2025-08-08 15:01:57 +0200

    feat: implement React Router to route each page of the web app

--- Travel/src/App.jsx
+++ Travel/src/App.jsx
@@ -1,40 +1,26 @@
 import './App.css'
-import { useEffect } from 'react'
-import { supabase } from './supabase/posts'
-import  PostForm  from  '../src/components/PostForm'
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import OutletLayout from './Pages/OutletLayout';
+import Home from './Pages/Home';
+import PostList from './Pages/PostList';
+import PostForm from './Pages/PostForm'
 
 
 function App() {
 
-  useEffect(() => {
-    const insertPost = async () => {
-      const newPost = {
-        foto_url: 'https://mi-foto.com/foto.jpg',
-        lugar: 'Barcelona, España',
-        description: 'Fui a la playa 🏖️',
-        mood_state: 'feliz',
-        spend: 30,
-        tags: ['playa', 'verano'],
-      }
 
-      const { data, error } = await supabase
-        .from('posts')
-        .insert([newPost])
-
-      if (error) console.error('Error insertando post:', error)
-      else console.log('Post insertado correctamente:', data)
-    }
-
-    insertPost()
-  }, [])
 
   return (
     <>
-      <div className='bg-gray-100 p-4'>
-        <h1 className='text-xl font-bold'>Travel Journal App</h1>
-      </div>
-      <PostForm />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<OutletLayout />}>
+            <Route index element={<Home />} />
+            <Route path="Lista-dei-Post" element={<PostList />} />
+            <Route path="Log-In" element={<PostForm />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
 
     </>
 
