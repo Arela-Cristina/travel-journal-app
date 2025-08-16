183ac90b71707f7cab6e62fb02db8961d1cfe780
Arela-Cristina <instagrandine@gmail.com>
2025-08-16 12:21:28 +0200

    feat: implement useParams to manage the id not found, and import the global context to popolate the Detail Page.

--- Travel/src/Pages/PostDetail.jsx
+++ Travel/src/Pages/PostDetail.jsx
@@ -1,5 +1,32 @@
+import { useParams } from "react-router-dom";
+import { usePosts } from "../Context/PostContext";
+
 export default function PostDetail() {
+
+    const { id } = useParams();
+    const { posts } = usePosts();
+
+    // find the post whit the respective id
+    const post = posts.find((p) =>  p.id === id);
+
+    if (!post) {
+        return <p className="text-center text-gray-500 mt-10">Post non trovato</p>;
+    }
+
     return (
-        <div>Detailx</div>
-    )
+
+        <div className="max-w-3xl mx-auto p-6">
+            <img
+                src={post.foto_url}
+                alt={post.place}
+                className="w-full h-[400px] object-cover rounded-lg shadow"
+            />
+            <h1 className="text-2xl font-bold mt-4">{post.place}</h1>
+            <p className="text-gray-700 mt-2">{post.description}</p>
+            <p className="text-sm text-gray-500 mt-2">
+                Data di Pubblicazione: {new Date(post.created_at).toLocaleDateString()}
+            </p>
+        </div>
+    );
 }
+
