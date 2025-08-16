4cec6635cfa6017f0a8f4679d63e3d15060993d3
Arela-Cristina <instagrandine@gmail.com>
2025-08-16 12:07:33 +0200

    feat: import PostDetail to App.jsx and implement react router to create a new route for each card

--- Travel/src/App.jsx
+++ Travel/src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import  PostProvider  from './Context/PostContextProvider';
+import PostProvider from './Context/PostContextProvider';
 import OutletLayout from './Pages/OutletLayout';
 import Home from './Pages/Home';
 import PostList from './Pages/PostList';
 import PostForm from './Pages/PostForm'
+import PostDetail from './Pages/PostDetail';
 
 
 function App() {
@@ -20,6 +21,7 @@ function App() {
             <Route path='/' element={<OutletLayout />}>
               <Route index element={<Home />} />
               <Route path="Lista-dei-Post" element={<PostList />} />
+              <Route path="/post/:id" element={<PostDetail />} />
               <Route path="Log-In" element={<PostForm />} />
             </Route>
           </Routes>
